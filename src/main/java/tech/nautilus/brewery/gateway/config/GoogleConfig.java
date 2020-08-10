dc02380276391a45ccfc22f458be125da40ed0e8
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2020-08-10 19:47:20 +0200

    Fixed test route (it was not working properly)

--- src/main/java/tech/nautilus/brewery/gateway/config/GoogleConfig.java
+++ src/main/java/tech/nautilus/brewery/gateway/config/GoogleConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 
 @Profile("google")
 @Configuration
@@ -15,7 +16,8 @@ public class GoogleConfig {
         return builder.routes()
                 .route("google",
                         r -> r.path("/googlesearch")
-                                .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                                .filters(f -> f.rewritePath("/googlesearch(?<segment>/.*)", "/${segment}").
+                                        redirect(HttpStatus.FOUND.value(), "https://www.google.com/"))
                                 .uri("https://www.google.com/"))
                 .build();
     }
